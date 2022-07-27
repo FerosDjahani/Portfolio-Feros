8ddcb7d4debf9055544a102ab70a942c005a088b
Feros Djahani <f.djahani@outlook.de>
2022-07-27 14:07:11 +0200

    first commit

---
+++ src/app/slideshow/slideshow.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-slideshow',
+  templateUrl: './slideshow.component.html',
+  styleUrls: ['./slideshow.component.scss']
+})
+export class SlideshowComponent implements OnInit {
+
+  images = ['wallpaperLaptop.jpg','wallpaperLaptop2.jpg'];
+  currentImage = 0;
+  showImage = true;
+
+ ngOnInit() {
+  this.updateImage();
+ }
+
+ updateImage(){
+
+  setInterval(() =>{
+    this.currentImage ++;
+    this.currentImage = this.currentImage % this.images.length;
+    this.showImage = false;
+
+    setTimeout(() => {
+      this.showImage = true;
+    } , 10);
+  }, 8000);
+
+ }
+
+}
